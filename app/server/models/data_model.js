56bf656ef36f1fe88aa8fd9fde001af75f7bd9d3
Caleb <agbenigacaleb@gmail.com>
2020-09-08 15:52:31 +0100

    Added initial JavaScript files to the Project Explorer Project

--- app/server/models/data_model.js
+++ app/server/models/data_model.js
@@ -8,7 +8,14 @@ class DataModel {
     }
 
     getById(id) {
+        for (var i = 0; i < this.data.length; i++) {
+            if (this.data[i].id == id) {
+                return this.data[i];
 
+            }
+        }
+
+        return null;
     }
 
     save(obj) {
@@ -21,10 +28,34 @@ class DataModel {
 
     update(obj, id) {
 
+        for (var i = 0; i < this.data.length; i++) {
+            if (this.data[i].id == id) {
+                let objKeys = Object.keys(obj);
+                for (var _keys = 0; _keys < objKeys.length; _keys++) {
+                    this.data[i][objKeys[_keys]] = obj[objKeys[_keys]];
+                }
+
+                return true;
+            }
+        }
+
+        return false;
     }
 
     delete(id) {
 
+        for (var i = 0; i < this.data.length; i++) {
+            if (this.data[i].id == id) {
+                const arrIndex = this.data.indexOf(this.data[i]);
+                if (arrIndex > -1) {
+                    this.data.splice(arrIndex, 1);
+                }
+                // delete this.data[i];
+                return true;
+            }
+        }
+
+        return false;
     }
 
     // this method will be overriden in the sub classes
--- app/server/models/users.js
+++ app/server/models/users.js
@@ -2,29 +2,74 @@ const DataModel = require('./data_model');
 
 class User {
     constructor(id, firstname, lastname, email, password, matricNumber, program, graduationYear) {
-
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.matricNumber = matricNumber;
+        this.program = program;
+        this.graduationYear = graduationYear;
     }
 
     getFullName() {
-
+        return this.firstname + " " + this.lastname;
     }
 }
 
 class Users extends DataModel {
     authenticate(email, password) {
+        var user = this.getByEmail(email);
 
+        if (user != null) {
+            if (user.password === password) {
+                return true;
+            }
+            return false;
+        }
+        return false;
     }
 
     getByEmail(email) {
+        for (var i = 0; i < this.data.length; i++) {
+            if (this.data[i].email == email) {
+                return this.data[i];
 
+            }
+        }
+
+        return null;
     }
 
     getByMatricNumber(matricNumber) {
+        for (var i = 0; i < this.data.length; i++) {
+            if (this.data[i].matricNumber == matricNumber) {
+                return this.data[i];
+            }
+        }
 
+        return null;
     }
 
     validate(obj) {
+        if (obj.firstname.length == 0 || obj.lastname.length == 0 || obj.email.length == 0 || obj.password.length == 0 || obj.matricNumber.length == 0 || obj.program.length == 0 || obj.graduationYear.length == 0) {
+            return false;
+        }
+        for (var i = 0; i < this.data.length; i++) {
+            if (this.data[i].email === obj.email) {
+                return false;
+            }
+        }
+        for (var i = 0; i < this.data.length; i++) {
+            if (this.data[i].matricNumber === obj.matricNumber) {
+                return false;
+            }
+        }
+        if (obj.password.length < 7) {
+            return false;
+        }
 
+        return true;
     }
 }
 
